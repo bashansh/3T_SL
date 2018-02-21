dfd1d2b11c1593c81efeddc5d6573660055842a1
bashansh <shmuel.bashan@gmail.com>
2018-02-21 11:35:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# 3T_SL
-terraform 3tier on Softlayer
+## 3-tier using Softlayer Classic resources
+
+
+## [Get the Terraform code](https://github.ibm.com/pbahrs/iaas_ref_architectures/tree/master/terraform/3Tier/ClassicSoftlayer)
+
+
+### Softlayer Classic Implementation Architecture Diagram
+
+   ![Burst virtual servers](https://github.ibm.com/pbahrs/iaas_ref_architectures/blob/master/imgs/SL3-tier.png)
+   
+### Architecture Notes  
+
+ - [IBM Cloud](https://www.ibm.com/cloud/?S_PKG=&cm_mmc=Search_Google-_-Cloud_Cloud+Platform-_-WW_US-_-+ibm++cloud_Broad_&cm_mmca1=000016GC&cm_mmca2=10004026&cm_mmca7=9008188&cm_mmca8=aud-311016886972:kwd-301973236515&cm_mmca9=6cd5a330-083d-4bc1-b6a9-9d15e42966fb&cm_mmca10=231586021885&cm_mmca11=b&mkwid=6cd5a330-083d-4bc1-b6a9-9d15e42966fb|1150|385392&cvosrc=ppc.google.%2Bibm%20%2Bcloud&cvo_campaign=000016GC&cvo_crid=231586021885&Matchtype=b)
+ 
+ - [Virtual Router Appliance](https://console.bluemix.net/docs/infrastructure/virtual-router-appliance/getting-started.html#getting-started)
+ 
+ - [VLAN Spanning](https://knowledgelayer.softlayer.com/procedure/enable-or-disable-vlan-spanning)
+ 
+ - [Data Center](https://www.ibm.com/cloud-computing/bluemix/data-centers)
+ 
+ - [Region](https://console.bluemix.net/docs/containers/cs_regions.html#regions-and-locations)
+ 
+ - [Public/Private VLAN](https://knowledgelayer.softlayer.com/topic/vlans)
+ 
+ - [Virtual Server Instance](https://www.ibm.com/cloud/virtual-servers)
+ 
+ - [Security Group](https://www.ibm.com/blogs/bluemix/2017/11/security-groups/)
+ 
+ - [Auto-scaling Group](https://www.ibm.com/cloud/auto-scaling)
+ 
+ - [IPSec VPN](https://www.ibm.com/blogs/bluemix/2016/07/vpn-for-bluemix-enhancements/)
+ 
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
+|Code organization|Used modules for each primary resource|
+|Single/multi-tenant hosts|The configuration file suports multi-tenant host deployments. To deploy as a single tenant, set the variable 'dedicated_acct_host_only" to "true" and include the host name.|
+|Number of VSIs to deploy|The configuration file supports two virtual servers in the initial auto-scaling group.|
+|Option to reuse existing VLAN|Included the "count" variable in the block to deploy the private VLAN. Also used the "replace" function to determine the value of the "public_vlan_id" in the "ibm_compute_vm_instance". The nested replace call returns the deployed vlan id when the vlan count is set to 1 and returns the vlanid variable when the count is set to 0.  ${replace(replace(var.vlan_count, "1", ibm_network_vlan.vlan_burst.id), "0", var.vlanid)} |
+|Local disk |Two SAN storage volumes for each VSI.|
+|Storage|A common file storage for web tier. A common file, block, object for app tier.|
+|Data services|Used IBM Cloudant data service.|
+|Security|A single, common security group for all VSIs in each of the two tiers. No firewall was used.|
+|SSH keys|Used a single, common key for all VSIs in each tier.|
+|Hostnaming|Used a naming convention that includes standard naming convention and adds the "count" value to the end of the name. ${format("burstvs-%02d", count.index + 1)} |
+|Limits on number of VSIs|Check with Softlayer capavbilities to understand the maximum numbers of VSIs that can be enterd into the count variable value| 
---
+++ main.tf
@@ -0,0 +1,109 @@
+# 3-tier MAIN.tf
+# This file runs each of the modules
+#
+
+#####################################################
+# Create Networks and subnets
+#####################################################
+
+module "network" {
+  source = "${var.network_location}"
+  datacenter = ${var.datacenter}"
+  public_router = ${var.public_router}
+  private_router = ${var.private_router} 
+}
+
+#####################################################
+# Create security groups
+#note this is not yet available in v0.5.1
+#####################################################
+
+#module "security" {
+#  source = "${var.security_location}"
+#  datacenter = ${var.datacenter}"
+#}
+
+#####################################################
+# Create Storage
+#####################################################
+module "storage" {
+  source = "${var.storage_location}"
+  datacenter = ${var.datacenter}"
+}
+#####################################################
+# Create webtier
+#####################################################
+module "webtier" {
+  source = "${var.vsi_location}"
+  datacenter = ${var.datacenter}"
+}
+#####################################################
+# Create apptier
+#####################################################
+module "apptier" {
+  source = "${var.vsi_location}"
+  datacenter = ${var.datacenter}"
+}
+#####################################################
+# Create data (service) tier
+#####################################################
+module "datatier" {
+  source = "${var.data_location}"
+  datacenter = ${var.datacenter}"
+}
+#####################################################
+# Variables
+#####################################################
+
+
+variable "datacenter" {
+  default = "dal06"
+  description = "the data center to deploy the VLAN."
+}
+variable "public_router" {
+  default = "fcr01a.dal06"
+  description = "the router to use for the public VLAN."
+}
+variable "private_router" {
+  default = "bcr01a.dal06"
+  description = "the router to use for the private VLAN."
+}
+variable "network_location" {
+  default = "https://github.ibm.com/pbahrs/iaas_ref_architectures/new/master/terraform/ClassicSoftLayer/3-tier/prototype/network"
+  description = "the network module location"
+}
+variable "security_location" {
+  default = "https://github.ibm.com/pbahrs/iaas_ref_architectures/new/master/terraform/ClassicSoftLayer/3-tier/prototype/security"
+  description = "the security module location"
+}
+variable "storage_location" {
+  default = "https://github.ibm.com/pbahrs/iaas_ref_architectures/new/master/terraform/ClassicSoftLayer/3-tier/prototype/storage"
+  description = "the storage module location"
+}
+variable "vsi_location" {
+  default = "https://github.ibm.com/pbahrs/iaas_ref_architectures/new/master/terraform/ClassicSoftLayer/3-tier/prototype/vsi"
+  description = "the vsi module location"
+}
+variable "data_location" {
+  default = "https://github.ibm.com/pbahrs/iaas_ref_architectures/new/master/terraform/ClassicSoftLayer/3-tier/prototype/data"
+  description = "the data module location"
+}
+#####################################################
+# Output reused as Variables
+#####################################################
+
+output "public_vlan_id" {
+  value = "${ibm_network_vlan.vlan_public.id}"
+}
+output "public_vlan_subnet_id" {
+  value = "${ibm_subnet.webtier_subnet.id}"
+}
+output "private_vlan_id" {
+  value = "${ibm_network_vlan.vlan_private.id}"
+}
+output "private_vlan_subnet1_id" {
+  value = "${ibm_subnet.apptier_subnet1.id}"
+}
+output "private_vlan_subnet2_id" {
+  value = "${ibm_subnet.apptier_subnet2.id}"
+}
